ce7b530896d22e56701ad61d8a5530e75503844a
spheleleMakau <missmakau9@gmail.com>
2024-05-23 15:10:21 +0200

    displaying items on the home page

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from item.models import Category , Item
 # Create your views here.
 def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
     # render template, this will be available inside the template
-    return render(request,'../templates/core/index.html' )
+    context={
+            'categories': categories,
+            'items':items, }
+    return render(request,'../templates/core/index.html',context=context)
 
 def contact(request):
     return render(request, "../templates/core/contact.html")
