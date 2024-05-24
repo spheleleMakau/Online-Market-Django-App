46416f4a27c926aeffe63aa15a102a43cb0cafb2
spheleleMakau <missmakau9@gmail.com>
2024-05-24 05:09:13 +0200

    added a sign up feature

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from item.models import Category , Item
+from . forms import SignupForm
+
 # Create your views here.
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -12,3 +14,20 @@ def index(request):
 
 def contact(request):
     return render(request, "../templates/core/contact.html")
+
+
+def singup(request):
+    if request.method =='POST':
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('/login')
+    else:
+       
+        form = SignupForm()
+    return render(request,'core/signup.html',
+                  {
+                      'form':form
+                  })
